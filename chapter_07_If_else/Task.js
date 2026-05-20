778ce8ee6d5ddda87a99807162f6c237bdbd00b1
Sampath-156 <sampath.aj156@gmail.com>
2026-05-20 07:51:32 +0530

    Update chapter 07 If else Task.js

--- chapter_07_If_else/Task.js
+++ chapter_07_If_else/Task.js
@@ -28,7 +28,7 @@ if (score > 90) {
 
 let year = 2020;    
 
-if (year % 4 === 0 && (year % 100 !== 0)) {
+if ((year % 4 === 0 && (year % 100 !== 0)) || year % 400 === 0)  {
     console.log("The year is a leap year.");
 } else {
     console.log("The year is not a leap year.");
